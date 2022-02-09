917d89317a73efc1aa607c78ddb9c915e0d6f2a8
ekball <90882418+ekball@users.noreply.github.com>
2022-02-09 11:53:53 -0500

    added ability to reply to comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -20,7 +20,35 @@ const commentController = {
                 res.json(dbPizzaData);
             })
             .catch(err => res.json(err));
-        },
+    },
+
+    // add a reply by updating a particular comment
+    addReply({ params, body }){
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
+    },
 
     // remove comment
     removeComment({ params }, res) {
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
-
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 // create comments through the pizza id
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // delete comments through the pizza id
+// add replies through the comment id
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// delete replies through the reply id
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 
 module.exports = router;
